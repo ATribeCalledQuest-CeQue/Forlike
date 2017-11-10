88007f7aa3b1504312c9bc5382c0ef483dd7d7a7
ATribeCalledQuest-Ce <slothman5339@gmail.com>
2017-11-09 20:59:05 -0800

    first commit

---
+++ Forlike/Interfaces/IActor.cs
@@ -0,0 +1,19 @@
+﻿using RogueSharp;
+using RLNET;
+namespace RogueSharpV3Tutorial.Interfaces
+{
+    public interface IActor
+    {
+        string Name { get; set; }
+        int Awareness { get; set; }
+    }
+    public interface IDrawable
+    {
+        RLColor Color { get; set; }
+        char Symbol { get; set; }
+        int X { get; set; }
+        int Y { get; set; }
+
+        void Draw(RLConsole console, IMap map);
+    }
+}
